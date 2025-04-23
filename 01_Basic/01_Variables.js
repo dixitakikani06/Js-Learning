37ce32233a79d2d8b850385b7c9d76b9bb90bdda
Chirag Prajapati <chirag.prajapati@nexuslinkservices.in>
2025-04-23 12:42:52 +0530

    variables

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 1445
+let accountEmail = "preksha@gmail.com"
+var accountPassword = "abc$$2378"
+acountCity = "Ahemdabad"
+let accountState;
+ //accountId = 70 //not allow by node 
+accountEmail = "ahs@gmail.com"
+accountPassword = "ehe##w21"
+acountCity = 34534.4
+console.log(accountId);
+console.table([accountEmail,accountPassword,acountCity,accountState])
+console.log(typeof acountCity); // "string"
+/*
+    prefer not to use var 
+    because of issue in block scope and functional scope 
+*/
