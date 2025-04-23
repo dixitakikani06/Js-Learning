db15368bd0cc69c7a708aae22a79661c21756188
Chirag Prajapati <chirag.prajapati@nexuslinkservices.in>
2025-04-23 18:31:20 +0530

    conversion datatype

---
+++ 01_Basic/03_conversionDatatype.js
@@ -0,0 +1,27 @@
+let score  = "4533sdsd";
+
+let valueInNum =  Number(score);
+//console.log(valueInNum);
+
+//console.table([typeof score, typeof(score), typeof(valueInNum)]);
+
+// "33" => 33
+// "44rewrw" resut => NAN
+// TRUE => 1 OR FALSE => 0
+let isLogIn = 1
+let isBoolenLogIn = Boolean(isLogIn);
+
+//console.log(isBoolenLogIn)
+//console.log(isLogIn);
+
+
+// 1 booln convert  true 1=> true , 0=> false
+// "" empty string convert in boolen  false
+// "sgfsjgf" convert in to boolen true
+
+
+let someNum = 5784353
+let someString = String(someNum);
+console.log(someNum);
+console.log(someString);
+console.log(typeof someString);
