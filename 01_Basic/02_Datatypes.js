530bfa9f8dec01c14e3bd04608e2a19f276113bd
Chirag Prajapati <chirag.prajapati@nexuslinkservices.in>
2025-04-23 16:54:09 +0530

    datatype

---
+++ 01_Basic/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat  all js code as newer version
+
+//alert(9+6) //we are ung node js not browser
+// console.log(5+7);    
+// console.log("ht");
+
+let isAvailble = true;
+let state;
+let name = "dixita";
+let phone = 9843535;
+
+//num => 2 to power 53
+//bigint
+//string => ""
+//boolen => true or false
+//null => standalone value
+//undefined
+//symbol => unique
+//object
+console.log(typeof "sweta");
+console.log(typeof phone);
+console.log(typeof null);
+console.log(typeof state);
