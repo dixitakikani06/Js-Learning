4b4e19346115a104964241e497dcd4c072a87e85
Chirag Prajapati <chirag.prajapati@nexuslinkservices.in>
2025-04-24 13:30:27 +0530

    primitive&non-primitive

---
+++ 01_Basic/04_conversionOperation.js
@@ -0,0 +1,39 @@
+let value  =  5;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log();
+// console.log(3+6);
+// console.log(7*9);
+// console.log(40-60);
+// console.log(10/2);
+// console.log(2**3);
+// console.log(10%2);
+
+// let str1 = "hllo";
+// let str2 = "dixita";
+
+// console.log(str1+str2);
+// console.log("4"+3 + 9);
+// console.log(4+6+"4"+3 );
+// console.log(4+(6+"4")+3 );
+// console.log(4%6+7-3);
+
+let num1 = num2 = num3 = 20;
+console.log(num2);
+
+// let gameCounter = 70;
+// gameCounter++;
+// console.log(gameCounter);
+
+// let gameCounter2 = 70;
+// gameCounter2--;
+// console.log(gameCounter2);
+
+// let gameCounter3 = 70;
+// -gameCounter3;
+// console.log(gameCounter3);
+
+// let gameCounter4 = 70;
+// --gameCounter4;
+// console.log(gameCounter3);
---
+++ 01_Basic/05_Datatype_summary.ts
@@ -0,0 +1,42 @@
+// primitive
+// 7types : string, Number, Boolen, null, Undefined, symbol, bigint
+//dcimal for not specific
+//const scoreNum = 100;
+//const scoreValue = 100.56;
+//const islog = false;
+//const outSideTemp = null;
+//let userEmail;
+//const UserId = Symbol("3213");
+//console.log(UserId);
+//const anotherId = Symbol("3213");
+//console.log(anotherId);
+
+//console.log(UserId == anotherId);
+
+const bigNumber = 5347568345483n;
+console.log(typeof bigNumber);
+
+//console.log(bigNumber);
+
+
+//non-primitive (reference)
+// array, Objects, Functions
+//non-primitive datatyp is object
+
+const fruits =  ["apple","banana","Mango"];
+console.log(fruits);
+
+let myObj = {
+    name: "dixita",
+    age : 22,
+    num : 249184
+}
+
+//store function into const varible
+const myFunction = function()
+{
+    console.log("hello sweta");
+}
+
+console.log(typeof myFunction);
+
